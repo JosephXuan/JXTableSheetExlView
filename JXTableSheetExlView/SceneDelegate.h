3d2a6cc82ee27d08d5d0a2fb9a5ca16fb3f7955c
chenzhe <chenzhe@baijiayun.com>
2019-10-30 18:24:55 +0800

    init

---
+++ JXTableSheetExlView/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  JXTableSheetExlView
+//
+//  Created by JosephXuan on 2019/10/30.
+//  Copyright © 2019 JosephXuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ JXTableSheetExlView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXTableSheetExlView
+//
+//  Created by JosephXuan on 2019/10/30.
+//  Copyright © 2019 JosephXuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
